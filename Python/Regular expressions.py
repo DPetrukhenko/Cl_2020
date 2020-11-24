54c9e4c8abc22774423a690769e5b7a58d404b0b
DPetrukhnenko <liriel777@yandex.ru>
2020-11-24 17:11:23 +0300

    добавлено домашнее задание по регулярным выражениям

---
+++ Python/Regular expressions.py
@@ -0,0 +1,21 @@
+import re
+# адреса
+
+addresses = open(r'addresses.txt', 'r', encoding="utf-8")
+addresses = addresses.read()
+addresses_extracted = re.findall(r"\n\d+.+\n.+\d+\n.+\d+", addresses)
+print(addresses_extracted)
+
+# прямая речь
+
+direct_speech = open(r'd_speech.txt', 'r', encoding="utf-8")
+direct_speech = direct_speech.read()
+d_speech_extracted = re.findall('—.+?—|«.+?—|—.+', direct_speech)
+print(d_speech_extracted)
+
+# морж-корж
+morzh = open(r'морж-корж.txt', 'r', encoding="utf-8")
+morzh = morzh.read()
+korzh1 = re.sub('морж', 'корж', morzh)
+korzh = re.sub('Морж', 'Корж', korzh1)
+print(korzh)
