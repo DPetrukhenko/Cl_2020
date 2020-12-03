bce95c9dc9ac50d4da4a8377a397ea48ad5b8bdd
DPetrukhnenko <liriel777@yandex.ru>
2020-12-03 14:13:19 +0300

    внесены правки в дз

--- Python/допольнительное дз по регулярным выражениям.py
+++ Python/допольнительное дз по регулярным выражениям.py
@@ -1,15 +1,15 @@
 import re
-with open('e-mails..txt', 'r', encoding='utf8') as file:
+with open(r'C:\Users\Антон\PycharmProjects\pythonProject\e-mails..txt', 'r', encoding='utf8') as file:
     text = file.read()
 
 # регулярное выражение для поиска полных адресов
 
-e_mails = re.findall(r'"? ?[\w.=\-\"!+]+@[\w.=\-\"!+]+', text)
+e_mails = re.findall(r'"? ?[\w.\-"!+]+@[\w.\-]*\w', text)
 e_mails = set(e_mails)
 print('Уникальных e-mail адресов в тексте найдено:', len(e_mails))
 
 # регулярное выражение для поиска доменов
 
-domains = re.findall(r'(?<=@)[\w+\-."]*\w', text)
+domains = re.findall(r'(?<=@)[\w\-.]*\w', text)
 domains = set(domains)
 print('Уникальных доменов в тексте найдено:', len(domains))
